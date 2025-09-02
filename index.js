60e788c99097e9eecf61501e9638757356288607
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-09-02 16:32:09 +0500

    sdfg

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+//string. number. boolean. null. undefined. symbol. bigint
+//Object -> array, function, object
+Object.defineProperty(exports, "__esModule", { value: true });
+// let title: string = 'Webbrain Academy';
+// let num = 1234
+// title = 'Webbrain IT Center';
+// num = 123
+// console.log(title);
+// let big: BigInt = 2134n;
+// big = 1234n;
+// console.log(big);
+let obj = {};
+obj = { name: 'Webbrain' };
+console.log(obj);
+//# sourceMappingURL=index.js.map
