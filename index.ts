2924d2bd7a08a3566b10e482b20351f266b066a9
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-09-03 12:43:07 +0500

    ssada

--- index.ts
+++ index.ts
@@ -31,4 +31,9 @@ let language: "uzbek" | "english" |"rus";
 
 const data:{id?:number, value:"uzbek" | "english" |"rus"}[] =[
     { id:1, value:"uzbek" },
-a
+    { id:2, value:"english" },
+    { id:3, value:"rus" },
+]
+
+data.map((lang) => {
+ a
