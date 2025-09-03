ead637c8695a3ef505d683d80826351bfac47040
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-09-03 12:41:10 +0500

    sadsa

--- index.ts
+++ index.ts
@@ -27,4 +27,14 @@
 // }
 // console.log(getData("webbrain",123).toUpperCase());
 
-asd
+let language: "uzbek" | "english" |"rus";
+
+const data:{id?:number, value:"uzbek" | "english" |"rus"}[] =[
+    { id:1, value:"uzbek" },
+    { id:2, value:"english" },
+    { id:3, value:"rus" },
+]
+
+data.map((lang) => {
+    language = lang.value;
+})
