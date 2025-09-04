20a9c3501a8ea816222746fc7db9698ec99c0054
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-09-04 10:21:35 +0500

    sadas

--- index.js
+++ index.js
@@ -2,5 +2,4 @@
 //string. number. boolean. null. undefined. symbol. bigint
 //Object -> array, function, object
 Object.defineProperty(exports, "__esModule", { value: true });
-// let title: string = 'Webbrain Academy';
 a
