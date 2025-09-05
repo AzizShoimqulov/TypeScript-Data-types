2618c78966d98298458e8ada966528b8f4f0e79e
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-09-05 10:30:11 +0500

    dwa

--- index.ts
+++ index.ts
@@ -1,18 +1,1 @@
-//string. number. boolean. null. undefined. symbol. bigint
-//Object -> array, function, object
-
-// let title: string = 'Webbrain Academy';
-// let num = 1234
-// title = 'Webbrain IT Center';
-// num = 123
-// console.log(title);
-
-
-
-
-// let big: BigInt = 2134n;
-// big = 1234n;
-// console.log(big);
-
-// let obj = {};
 a
